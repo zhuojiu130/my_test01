9b035bba81eabea542e807975ebd0a500e24bde3
zhuojiu_130 <13048402619@163.com>
2024-10-02 16:45:16 +0800

    -7

--- test01/test_3_shuzu.c
+++ test01/test_3_shuzu.c
@@ -17,15 +17,42 @@
 //	printf("二维数组的行数:%d\n", sizeof(arr) / sizeof(arr[0]));
 //	printf("二维数组的列数:%d\n", sizeof(arr[0]) / sizeof(arr[0][0]));
 //
+//	//可以查看二维数组的首地址
+//	printf("二维数组的首地址:%d\n", arr);
+//	printf("二维数组的第一行地址:%d\n", arr[0]);
+//	printf("二维数组的第二行地址:%d\n", arr[1]);
+//	printf("二维数组的第一元素的地址:%d\n", &arr[0][0]);
+//	printf("二维数组的第二元素的地址:%d\n", &arr[0][1]);
+//
 //}
 //
+////二维数组的案例 求总和成绩
 //void test02()
 //{
+//	int scores[3][3] =
+//	{
+//		{100, 100, 100},//第一个人成绩
+//		{90, 50, 80},	//第二个人成绩
+//		{60, 70, 80}	//第三个人成绩
+//	};
 //
+//	int row = sizeof(scores) / sizeof(scores[0]);	//获取行
+//	int col = sizeof(scores[0]) / sizeof(scores[0][0]);	//获取列
 //
+//	for (int i = 0; i < row; i++)
+//	{
+//		int sum = 0;
+//		int pjun = 0;
+//		for (int j = 0; j < col; j++)
+//		{
+//			sum += scores[i][j];//计算总分
+//		}
+//		double avg = (double)sum / col; // 计算平均分	//(double)sum为强制类型转换	//%.2f：占位符，表示输出一个浮点数，并保留两位小数。
+//		printf("第%d个同学总成绩为:%d, 平均分为:%.2f\n", i + 1, sum, avg);
+//	}
 //}
 //int main()
 //{
-//	test01();
+//	test02();
 //	return 0;
 //}
--- test01/test_4.c
+++ test01/test_4.c
@@ -1,90 +1,90 @@
-#include <stdio.h>
-#include <windows.h>	// 调用 Sleep() 函数
-
-// 显示菜单函数
-void menu()
-{
-	printf("===== 学生成绩管理系统 =====\n");
-	printf("1. 学生成绩录入\n");
-	printf("2. 学生成绩修改\n");
-	printf("3. 学生成绩删除\n");
-	printf("4. 学生成绩查询\n");
-	printf("5. 学生成绩打印\n");
-	printf("0. 退出系统\n");
-	printf("===========================\n");
-}
-
-//成绩录入函数
-void input_score()
-{
-	printf("准备成绩录入......\n");
-	//定义3人成绩的变量
-	int stu1 = 0;
-	int stu2 = 0;
-	int stu3 = 3;
-	//接收3个人的成绩
-	printf("请输入第一个人的分数:");
-	scanf("%d", &stu1);
-
-	printf("请输入第二个人的分数:");
-	scanf("%d", &stu2);
-
-	printf("请输入第三个人的分数:");
-	scanf("%d", &stu3);
-
-	//打印三人的成绩
-	printf("第一个学生的成绩:%d\n", stu1);
-	printf("第二个学生的成绩:%d\n", stu2);
-	printf("第三个学生的成绩:%d\n", stu3);
-}
-
-
-// 用户选择的函数
-void test02()
-{
-	int choose = -1; // 初始化选择为 -1，确保进入循环
-
-	while (choose != 0)
-	{
-		menu(); // 显示菜单
-
-		printf("请输入你的选择 (0-5):> ");
-		scanf("%d", &choose);
-
-		switch (choose)
-		{
-		case 1:
-			printf("正在使用：学生成绩录入\n");
-			input_score();	//调用函数
-			break;
-		case 2:
-			printf("正在使用：学生成绩修改\n");
-			break;
-		case 3:
-			printf("正在使用：学生成绩删除\n");
-			break;
-		case 4:
-			printf("正在使用：学生成绩查询\n");
-			break;
-		case 5:
-			printf("正在使用：学生成绩打印\n");
-			break;
-		case 0:
-			printf("谢谢使用，再见！\n");
-			break;
-		default:
-			printf("对不起，没有这个菜单项！请重新输入。\n");
-			break;
-		}
-
-		Sleep(2500); // 稍作暂停
-
-		system("cls"); // 清屏，重新显示菜单
-	}
-}
-
-int main()
-{
-	test02(); // 调用处理函数
-	return 0;
-}
+//#include <stdio.h>
+//#include <windows.h>	// 调用 Sleep() 函数
+//
+//// 显示菜单函数
+//void menu()
+//{
+//	printf("===== 学生成绩管理系统 =====\n");
+//	printf("1. 学生成绩录入\n");
+//	printf("2. 学生成绩修改\n");
+//	printf("3. 学生成绩删除\n");
+//	printf("4. 学生成绩查询\n");
+//	printf("5. 学生成绩打印\n");
+//	printf("0. 退出系统\n");
+//	printf("===========================\n");
+//}
+//
+////成绩录入函数
+//void input_score()
+//{
+//	printf("准备成绩录入......\n");
+//	//定义3人成绩的变量
+//	int stu1 = 0;
+//	int stu2 = 0;
+//	int stu3 = 3;
+//	//接收3个人的成绩
+//	printf("请输入第一个人的分数:");
+//	scanf("%d", &stu1);
+//
+//	printf("请输入第二个人的分数:");
+//	scanf("%d", &stu2);
+//
+//	printf("请输入第三个人的分数:");
+//	scanf("%d", &stu3);
+//
+//	//打印三人的成绩
+//	printf("第一个学生的成绩:%d\n", stu1);
+//	printf("第二个学生的成绩:%d\n", stu2);
+//	printf("第三个学生的成绩:%d\n", stu3);
+//}
+//
+//
+//// 用户选择的函数
+//void test02()
+//{
+//	int choose = -1; // 初始化选择为 -1，确保进入循环
+//
+//	while (choose != 0)
+//	{
+//		menu(); // 显示菜单
+//
+//		printf("请输入你的选择 (0-5):> ");
+//		scanf("%d", &choose);
+//
+//		switch (choose)
+//		{
+//		case 1:
+//			printf("正在使用：学生成绩录入\n");
+//			input_score();	//调用函数
+//			break;
+//		case 2:
+//			printf("正在使用：学生成绩修改\n");
+//			break;
+//		case 3:
+//			printf("正在使用：学生成绩删除\n");
+//			break;
+//		case 4:
+//			printf("正在使用：学生成绩查询\n");
+//			break;
+//		case 5:
+//			printf("正在使用：学生成绩打印\n");
+//			break;
+//		case 0:
+//			printf("谢谢使用，再见！\n");
+//			break;
+//		default:
+//			printf("对不起，没有这个菜单项！请重新输入。\n");
+//			break;
+//		}
+//
+//		Sleep(2500); // 稍作暂停
+//
+//		system("cls"); // 清屏，重新显示菜单
+//	}
+//}
+//
+//int main()
+//{
+//	test02(); // 调用处理函数
+//	return 0;
+//}
